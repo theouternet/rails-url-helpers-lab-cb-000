b5074085a517d86bbee81ccd96c22e09693af11e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-23 04:15:24 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,10 +7,12 @@ class StudentsController < ApplicationController
 
   def show
   end
-
+ 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active 
+    @student.save
+    redirect_to student_path(@student)
+  end
   
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
